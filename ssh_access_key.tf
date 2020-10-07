88754a2aa35f3872d37431f83ce482b440e75b88
shrek19937 <qing.wu@gmail.com>
2020-10-06 22:46:28 -0500

    init version

---
+++ ssh_access_key.tf
@@ -0,0 +1,6 @@
+# Create (and display) an SSH key
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+output "tls_private_key" { value = tls_private_key.example_ssh.private_key_pem }
